8fe07ed9981a3c64ead64c2b5129c61aad985187
shankar1598 <shankar15498@gmail.com>
2019-03-09 19:53:14 +0530

    Yay! You are on minikube cluster with env based config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
-
 FROM ruby:2.6.0
 
 RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
@@ -6,9 +5,17 @@ RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
 RUN mkdir /fakeapp
 WORKDIR /fakeapp
 
-ADD Gemfile Gemfile
-ADD Gemfile.lock Gemfile.lock
+COPY Gemfile Gemfile
+COPY Gemfile.lock Gemfile.lock
 
 RUN bundle install
 
-ADD . /fakeapp
+COPY . .
+
+# RUN bundle exec rake db:create
+
+ENTRYPOINT [ "./docker-entrypoint.sh" ]
+
+
+CMD ["rails", "s"]
+# CMD ["echo", "CMD COMMAND RUNNING"]
---
+++ init.sh
@@ -0,0 +1,15 @@
+# export TAGALYS_SECRET = $(cat /mnt/secret/TAGALYS_SECRET)
+# export DB_USER = $(cat /mnt/secret/DB_USER)
+# export DB_NAME = $(cat /mnt/secret/DB_NAME)
+# export DB_PASSWORD = $(cat /mnt/secret/DB_PASSWORD)
+# export DB_HOST  = $(cat /mnt/secret/DB_HOST)
+# export DEV_SECRET_KEY = $(cat /mnt/secret/DEV_SECRET_KEY)
+# export PROD_SECRET_KEY = $(cat /mnt/secret/PROD_SECRET_KEY)
+# export TEST_SECRET_KEY = $(cat /mnt/secret/TEST_SECRET_KEY)
+# export MYSQL_ROOT_PASSWORD = $(cat /mnt/secret/MYSQL_ROOT_PASSWORD)
+# export MYSQL_DATABASE = $(cat /mnt/secret/MYSQL_DATABASE)
+# export MYSQL_USER = $(cat /mnt/secret/MYSQL_USER)
+# export MYSQL_PASSWORD = $(cat /mnt/secret/MYSQL_PASSWORD)
+# export MYSQL_ROOT_HOST = $(cat /mnt/secret/MYSQL_ROOT_HOST)
+
+echo "SET ENV DONE"
