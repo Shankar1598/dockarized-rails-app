f10c9c15cf605bfcd4f7b92509823ff6e603f9ac
shankar1598 <shankar15498@gmail.com>
2019-03-10 15:31:40 +0530

    removed docker push

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-docker build -t shankar1598/tagalys-api-server:latest -t shankar1598/tagalys-api-server:$SHA .
-docker push shankar1598/tagalys-api-server:latest
-docker push shankar1598/tagalys-api-server:$SHA
+# docker build -t shankar1598/tagalys-api-server:$SHA .
+# docker tag shankar1598/tagalys-api-server:$SHA shankar1598/tagalys-api-server:latest
+# docker push shankar1598/tagalys-api-server:latest
+# docker push shankar1598/tagalys-api-server:$SHA
 kubectl apply -k8s
-kubectl set image deployments/tagalys-api-server api-server-container=shankar1598/tagalys-api-server:$SHA
+kubectl set image deployments/tagalys-api-server api-server-container=shankar1598/tagalys-api-server:v13
