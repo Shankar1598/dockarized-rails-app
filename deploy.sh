ec209c6dab901ea8c53950c5971972ccc6f16cb1
shankar1598 <shankar15498@gmail.com>
2019-03-13 01:20:38 +0530

    travis fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# docker build -t shankar1598/tagalys-api-server:$SHA .
-# docker tag shankar1598/tagalys-api-server:$SHA shankar1598/tagalys-api-server:latest
-# docker push shankar1598/tagalys-api-server:latest
-# docker push shankar1598/tagalys-api-server:$SHA
+docker build -t shankar1598/tagalys-api-server:$SHA .
+docker tag shankar1598/tagalys-api-server:$SHA shankar1598/tagalys-api-server:latest
+docker push shankar1598/tagalys-api-server:latest
+docker push shankar1598/tagalys-api-server:$SHA
 echo "Trying to apply kubectl"
 kubectl apply -f k8s
 kubectl set image deployments/tagalys-api-server api-server-container=shankar1598/tagalys-api-server:v13
